201b6970a2b282da819bca94a9ea109a411c0cfa
Samaelish <semnd007@gmail.com>
2021-04-22 17:40:07 +0300

    basic fetch + useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json)) 
+  }, [resourceType])
+
   return (
     <>
       <div>
@@ -10,6 +18,9 @@ function App() {
         <button onClick={() => setResourceType('comments')}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre key={item.id}>{JSON.stringify(item)}</pre>
+      })}
     </>
   );
 }
