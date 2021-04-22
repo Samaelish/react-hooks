530baa6f9b641904af8a3064b2062f86001069a3
Samaelish <semnd007@gmail.com>
2021-04-22 18:48:22 +0300

    useref for saving prevstate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import UseRef from './UseRef'
+import UseRef2 from './UseRef2'
 
 function App() {
   return (
     <>
-      <UseRef />
+      <UseRef2 />
     </>
   )
 }
---
+++ src/UseRef2.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useRef } from 'react'
+
+function UseRef2() {
+  const [name, setName] = useState('')
+  const prevName = useRef('')
+  
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
+
+  return (
+    <>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+      <div>My name is {name} and it used to be {prevName.current}</div>
+    </>
+  )
+}
+
+export default UseRef2;
