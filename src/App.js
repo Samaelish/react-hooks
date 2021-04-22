9d585bebc2a3cb3d6dba091a1130e2ab7d71b9d5
Samaelish <semnd007@gmail.com>
2021-04-22 17:27:53 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  function decrementCount() {
-    setCount(prevCount => prevCount - 1)
-  }
-  function incrementCount() {
-    setCount(prevCount => prevCount + 1)
-  }
+  const [resourceType, setResourceType] = useState('posts')
   return (
-    <div className="App">
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
-    </div>
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
   );
 }
 
